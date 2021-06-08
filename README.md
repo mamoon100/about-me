23dae15d7aa2c1290a90d85538f70c9d392742fb
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-06-08 16:25:01 +0300

    added the 6th and 7th qustion for the game

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 ## About me game takes gussies from the user and check if it's correct or not by the ``if`` statement.
 
+-----
+
+* Edit 08/06/2021:
+Added some new **``prombt``** and do the while idea to check counter and answers.   
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let correctAnswer = 0;
 const userName = prompt('Please enter your Name');
 alert (`Welcome to my Website ${userName}`);
 let coffeeDrink = prompt ('Do you think i like coffee?').toLowerCase();
@@ -10,6 +10,7 @@ if (coffeeDrink === 'yes' || coffeeDrink === 'y') {
 else if (coffeeDrink === 'no' || coffeeDrink === 'n') {
 //   console.log('you are correct');
   alert('you are correct');
+  correctAnswer++;
 }
 else {
   alert('please choose between yes/y and no/n');
@@ -22,6 +23,7 @@ if (driveCar === 'yes' || driveCar === 'y') {
 else if (driveCar === 'no' || driveCar === 'n') {
   //   console.log('you are correct');
   alert('you are correct');
+  correctAnswer++;
 }
 else {
   alert('please choose between yes/y and no/n');
@@ -30,6 +32,7 @@ let internetUsage = prompt ('Do you think i spend a lot of time on the internet?
 if (internetUsage === 'yes' || internetUsage === 'y') {
   // console.log('you are correct');
   alert('you are correct');
+  correctAnswer++;
 }
 else if (internetUsage === 'no' || internetUsage === 'n') {
   //   console.log('you not are correct');
@@ -42,6 +45,7 @@ let introvert = prompt ('Do you think i\'m an introvert?').toLowerCase();
 if (introvert === 'yes' || introvert === 'y') {
   // console.log('you are correct');
   alert('you are correct');
+  correctAnswer++;
 }
 else if (introvert === 'no' || introvert === 'n') {
   //   console.log('you are not correct');
@@ -54,6 +58,7 @@ let stuffedAnimal = prompt('Do you think i have stuffed animal called bakreek').
 if (stuffedAnimal === 'yes' || stuffedAnimal === 'y') {
   // console.log('you are correct');
   alert('you are correct');
+  correctAnswer++;
 }
 else if (stuffedAnimal === 'no' || stuffedAnimal === 'n') {
   //   console.log('you are not correct');
@@ -64,3 +69,62 @@ else {
 }
 
 alert(`Thank you ${userName} for playing with me`);
+
+let counter = 1;
+let theNumber = Math.floor(Math.random()*100);
+let takeGuess = prompt('Guess a number between 0 and 99');
+while (typeof(parseInt(takeGuess)) !== 'number') {
+  takeGuess = prompt('Guess a number between 0 and 99');
+}
+
+do {
+  if (takeGuess === theNumber) {
+    alert(`${userName} You have won ${theNumber} is the correct number \n you have ${4-counter} try left`);
+    correctAnswer++;
+    break;
+  }
+  else if (takeGuess > theNumber) {
+    takeGuess = prompt (`try something lower \n you still have ${4-counter} try left`);
+    counter++;
+
+  }
+  else if (takeGuess < theNumber) {
+    takeGuess = prompt (`try something higher \n you still have ${4-counter} try left `);
+    counter++;
+
+  }
+} while (counter < 4);
+
+if (counter >= 4) {
+  alert(`The correct answer is ${theNumber}`);
+}
+
+let periodicTable = ['h', 'he', 'li', 'be', 'b', 'c', 'n', 'o', 'f', 'ne', 'na', 'mg', 'al', 'si', 'p', 's', 'cl', 'ar', 'k', 'ca'];
+let periodicCounter = 1;
+let nameGuess = prompt('Guess which periodic table element im thinking of');
+let isItThere = false;
+
+do {
+  for (let i = 0; i<periodicTable.length; i++){
+    if (periodicTable[i] === nameGuess) {
+      isItThere = true;
+      break;
+    }
+  }
+  if (isItThere) {
+    alert(`You have Guessed it Right \n The full Answer is \n ${periodicTable} \n you still have ${6-periodicCounter} try left`);
+    correctAnswer++;
+    break;
+  }
+  else {
+    periodicCounter++;
+    nameGuess = prompt(`try again \n Hint: periodic Table 20 items \n you still have ${7-periodicCounter} Gussies left`);
+  }
+} while (periodicCounter <= 6 );
+
+if (periodicCounter > 6) {
+  alert(`You have lost the correct answers are \n ${periodicTable}`);
+}
+
+
+alert (`you have made it to the scores area \n you scored ${correctAnswer} out of 7`);
